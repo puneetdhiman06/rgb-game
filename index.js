826d16675a624d015a2b2de87018d883456e65fe
puneet <puneetdhiman.06@gmail.com>
2020-07-31 13:11:40 +0530

    random color for each square

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function refresh()
-{
+{   //color generator
     function generator()
         {return Math.floor(Math.random()*255);}
+
+    //to display on webpage    
       
     let r=generator()
     let g=generator()
@@ -10,6 +12,17 @@ function refresh()
     document.getElementById("r").innerHTML = r;
     document.getElementById("g").innerHTML = g;
     document.getElementById("b").innerHTML = b;
+
+    //for random color for each square
+
+    for(let i = 1 ; i<=6 ; i++)
+    {
+        let q = generator()
+        let w = generator()
+        let e = generator()
+        
+        document.getElementById(`sq${i}`).style.backgroundColor = `rgb(${q}, ${w}, ${e})`
+    }
 
     
 }
